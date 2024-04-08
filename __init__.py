27bf33f62cf09b3803b7fcfcc3dcf8f0ea7b553b
joselourenco <3s3phinxil@gmail.com>
2024-04-08 20:58:29 -0300

    chore: cors

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from rotas import bp_rotas
 
 app = Flask(__name__)
+CORS(app)
 
 # Registrar as rotas do blueprint
 app.register_blueprint(bp_rotas)
